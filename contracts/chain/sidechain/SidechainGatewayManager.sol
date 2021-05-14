f9a42cb11b8512ed0e2398c011f181da78bd9536
Duc Tho Tran <ducthotran2010@gmail.com>
2021-05-14 14:18:15 +0700

    Update fallback method of Sidechain Gateway Manager contract

--- contracts/chain/sidechain/SidechainGatewayManager.sol
+++ contracts/chain/sidechain/SidechainGatewayManager.sol
@@ -41,8 +41,9 @@ contract SidechainGatewayManager is SidechainGatewayStorage {
 
   function()
     external
-    payable
-  {}
+  {
+    revert("SidechainGatewayManager: Invalid method");
+  }
 
   function batchDepositERCTokenFor(
     uint256[] calldata _depositIds,
